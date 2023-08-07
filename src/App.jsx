6133346b9f9bfcd16b6dd1c84c9ff07c940c2118
Ivana Pusic <pusicka97@gmail.com>
2023-08-07 17:35:24 +0200

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-function App() {
-  return (
+import {} from 'react';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import {
+  AddNewCampground,
+  AddNewComment,
+  AppLayout,
+  Campgrounds,
+  ErrorPage,
+  LandingPage,
+  SignInPage,
+  SignupPage,
+  SingleCampground,
+} from './pages';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
     <>
-      <h1>Yelp Camp</h1>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/campgrounds' element={<AppLayout />}>
+        <Route index element={<Campgrounds />} />
+        <Route path=':id' element={<SingleCampground />} />
+        <Route path=':id/comment' element={<AddNewComment />} />
+        <Route path='add-new-campground' element={<AddNewCampground />} />
+      </Route>
+      <Route path='sign-in' element={<SignInPage />} />
+      <Route path='sign-up' element={<SignupPage />} />
+      <Route path='*' element={<ErrorPage />} />
     </>
-  );
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import AddNewCampground from './add-new-campground/AddNewCampground';
 import AddNewComment from './add-new-comment/AddNewComment';
+import Campgrounds from './campgrounds/Campgrounds';
 import ErrorPage from './error-page/ErrorPage';
 import LandingPage from './landing-page/LandingPage';
+import AppLayout from './layout/AppLayout';
 import SearchPage from './search-page/SearchPage';
 import SignInPage from './signin-page/SignInPage';
 import SignupPage from './signup-page/SignupPage';
@@ -10,6 +12,8 @@ import SingleCampground from './single-campground/SingleCampground';
 export {
   AddNewCampground,
   AddNewComment,
+  AppLayout,
+  Campgrounds,
   ErrorPage,
   LandingPage,
   SearchPage,
